050996a2d43bf02f16b3a0f826f38f26965142eb
Nzzz964 <2582787306@qq.com>
2022-04-24 09:11:51 +0800

    fix: known bugss

--- spider/spider.go
+++ spider/spider.go
@@ -6,13 +6,14 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/gocolly/colly"
 	"github.com/gocolly/colly/proxy"
 	"github.com/gocolly/colly/queue"
 )
 
-var gSpider *Spider = nil
+var ua = colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
 
 type Spider struct {
 	//目标URL
@@ -29,23 +30,22 @@ type Spider struct {
 	Collector *colly.Collector
 }
 
+func collyFactory() *colly.Collector {
+	return colly.NewCollector(
+		ua,
+	)
+}
+
 func NewSpider(u string, r string) *Spider {
-	gSpider = &Spider{
+	return &Spider{
 		Target: u,
 		Range:  r,
 		Thread: 8,
 		Headers: map[string]string{
 			"Referer": "https://www.cartoonmad.com",
 		},
-		Collector: colly.NewCollector(
-			colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"),
-		),
+		Collector: collyFactory(),
 	}
-	return gSpider
-}
-
-func GetSpider() *Spider {
-	return gSpider
 }
 
 func (s *Spider) SetThread(t int) *Spider {
@@ -56,75 +56,78 @@ func (s *Spider) SetThread(t int) *Spider {
 //设置代理 Set Colly Proxy
 func (s *Spider) SetProxy(p string) *Spider {
 	s.Proxy = p
-	rp, _ := proxy.RoundRobinProxySwitcher(p)
-	s.Collector.SetProxyFunc(rp)
+	proxyFunc, _ := proxy.RoundRobinProxySwitcher(p)
+	s.Collector.SetProxyFunc(proxyFunc)
 	return s
 }
 
 //获取Comic对象
 func (s *Spider) GetComic() (*Comic, error) {
-	c := s.Collector
-	cm := Comic{}
+	collector := s.Collector
+	comic := Comic{}
 
-	c.OnRequest(func(r *colly.Request) {
+	collector.OnRequest(func(r *colly.Request) {
 		for k, v := range s.Headers {
 			r.Headers.Set(k, v)
 		}
 	})
 
-	c.OnHTML("body > table > tbody > tr:nth-child(1) > td:nth-child(2) > table > tbody > tr:nth-child(3) > td:nth-child(2) > a:nth-child(6)",
+	//获取 Title
+	collector.OnHTML("td:nth-child(2) > a:nth-child(6)",
 		func(e *colly.HTMLElement) {
 			b, _ := DecodeBig5([]byte(e.Text))
-			cm.Title = b
+			comic.Title = b
 		},
 	)
-	//Get all chapter
-	c.OnHTML("table:nth-child(3) table td a",
+	//获取所有章节
+	collector.OnHTML("table:nth-child(3) table td a",
 		func(e *colly.HTMLElement) {
 			matches := regexp.MustCompile(
 				`^/comic/(\d{4})(\d)(\d{3})(\d{4})(\d{3}).html$`,
 			).FindStringSubmatch(e.Attr("href"))
-			cm.Urls = append(cm.Urls, "https://www.cartoonmad.com/comic/comicpic.asp?file=/"+matches[1]+"/"+matches[3]+"/")
-			cm.Total++
+			comic.Urls = append(comic.Urls, "https://www.cartoonmad.com/comic/comicpic.asp?file=/"+matches[1]+"/"+matches[3]+"/")
+			comic.Total++
 		},
 	)
-
-	c.OnHTML("table:nth-child(3) table td font",
+	//获取页数
+	collector.OnHTML("table:nth-child(3) table td font",
 		func(e *colly.HTMLElement) {
-			t, _ := DecodeBig5([]byte(e.Text))
-			t = regexp.MustCompile(`\d+`).FindString(t)
-			i, _ := strconv.Atoi(t)
-			cm.Pages = append(cm.Pages, i)
+			text, _ := DecodeBig5([]byte(e.Text))
+			match := regexp.MustCompile(`\d+`).FindString(text)
+			page, _ := strconv.Atoi(match)
+			comic.Pages = append(comic.Pages, page)
 		},
 	)
 
-	if e := c.Visit(s.Target); e != nil {
+	if err := collector.Visit(s.Target); err != nil {
 		fmt.Printf("request: %s error\n", s.Target)
 		os.Exit(1)
 	}
 
 	rg := strings.Split(s.Range, "-")
-	length := len(cm.Urls)
+	length := len(comic.Urls)
 	start, _ := strconv.Atoi(rg[0])
 	end, _ := strconv.Atoi(rg[1])
+
 	if 0 < start && start <= length && start <= end && end <= length {
 		start--
-		cm.Urls = cm.Urls[start:end]
-		cm.Pages = cm.Pages[start:end]
-		return &cm, nil
+		comic.Urls = comic.Urls[start:end]
+		comic.Pages = comic.Pages[start:end]
+		return &comic, nil
 	}
-	return &cm, fmt.Errorf("目标章节范围不正确 min:%d max:%d", 1, length)
+
+	return &comic, fmt.Errorf("目标章节范围不正确 min:%d max:%d", 1, length)
 }
 
 func (s *Spider) Start() {
-	c := colly.NewCollector(
-		colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"),
-	)
+	collector := collyFactory()
+
 	if s.Proxy != "" {
-		rp, _ := proxy.RoundRobinProxySwitcher(s.Proxy)
-		c.SetProxyFunc(rp)
+		proxyFunc, _ := proxy.RoundRobinProxySwitcher(s.Proxy)
+		collector.SetProxyFunc(proxyFunc)
 	}
-	q, _ := queue.New(
+
+	queue, _ := queue.New(
 		s.Thread,
 		&queue.InMemoryQueueStorage{MaxSize: 10000},
 	)
@@ -139,30 +142,32 @@ func (s *Spider) Start() {
 	title := comic.Title
 	chapters := comic.GetChapter()
 
-	c.OnRequest(func(r *colly.Request) {
+	collector.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("Referer", "https://www.cartoonmad.cc/")
 		r.Headers.Set("Host", "www.cartoonmad.com")
 	})
 
-	//ProcessBar
 	bar := Bar{}
-	//Downloaded Count
-	dCount := 0
+	downloads := 0
+	var mutex sync.Mutex
 
-	c.OnResponse(func(r *colly.Response) {
+	collector.OnResponse(func(r *colly.Response) {
 		url := fmt.Sprint(r.Request.URL)
-		matches := regexp.MustCompile(`^https:\/\/www\.cartoonmad\.com\/.*?\/.*?\/(\d*)\/(.*)$`).FindStringSubmatch(url)
-		// Download path
-		dp := strings.Join([]string{"./downloads/", title, "/第", matches[1], "话/"}, "")
-		if _, err := os.Stat(dp); os.IsNotExist(err) {
-			os.MkdirAll(dp, 0755)
+		matches := regexp.MustCompile(`^https:\/\/.*?\/.*?\/.*?\/(\d*)\/(.*)$`).FindStringSubmatch(url)
+
+		osPath := strings.Join([]string{"./downloads/", title, "/第", matches[1], "话/"}, "")
+		if _, err := os.Stat(osPath); os.IsNotExist(err) {
+			os.MkdirAll(osPath, 0755)
 		}
-		r.Save(dp + matches[2])
-		dCount++
-		bar.Play(int64(dCount))
+		r.Save(osPath + matches[2])
+
+		mutex.Lock()
+		downloads++
+		bar.Play(int64(downloads))
+		mutex.Unlock()
 	})
 
-	c.OnError(func(r *colly.Response, e error) {
+	collector.OnError(func(r *colly.Response, e error) {
 		fmt.Printf("\nrequest: %s error retrying...\n", fmt.Sprint(r.Request.URL))
 		r.Request.Retry()
 	})
@@ -171,12 +176,12 @@ func (s *Spider) Start() {
 	for _, chapter := range chapters {
 		for _, url := range chapter.Urls {
 			count++
-			q.AddURL(url)
+			queue.AddURL(url)
 		}
 	}
 
 	bar.NewOptionWithGraph(0, int64(count), "#")
-	q.Run(c)
+	queue.Run(collector)
 
 	fmt.Println()
 }
